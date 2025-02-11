cc9d5cab72a4787b5ae51b7868d9d2877091b220
Alexey <alexey.zaytsev@gmail.com>
2025-02-11 11:52:27 -0300

    forgot the code

---
+++ nbs/day_05_matrix-vector.cu
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdio.h>
+
+__global__ void mat_vec_mul(float* m, float* v, float* res,
+                            uint32_t m_height,
+                            uint32_t m_width) {
+
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float out;
+    if (y < m_height) {
+        out = 0;
+        for (int i = 0; i < m_width; i++) {
+            out += m[y * m_width + i] * v[i];
+        }
+        res[y] = out;
+    }
+}
+
+
